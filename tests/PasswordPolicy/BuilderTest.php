af78a360b6543e896d4c96f163917d72a5624c64
John Eckhardt <john@codepros.biz>
2021-06-18 09:07:13 -0500

    match proper function signature

--- tests/PasswordPolicy/BuilderTest.php
+++ tests/PasswordPolicy/BuilderTest.php
@@ -22,7 +22,7 @@ class BuilderTest extends TestCase
      */
     private $failingRule;
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
         $this->passingRule = $this->createMock(Uppercase::class);
